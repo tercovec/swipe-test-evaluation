0057daacdaa2023ef3a74e2bc69e260695cb26a3
petr.z@outlook.com <petr.z@outlook.com>
2023-10-12 14:18:41 +0000

    Edited ServerModule1 and main.graphs

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -10,13 +10,13 @@ def send_mail(graphs):
     tag = ''
     for graph, graph_description in graphs:
       "creates a html tag containing all the images encoded in base64"
-      tag += f'''<figure><img alt="Swipe result" style="width:95%;height:auto" src="data:image/jpeg;base64,{helpers.get_b64string(graph.get_bytes())}"><br><figcaption>{graph_description or ''}</figcaption></figure>'''
+      tag += f'''<figure><img alt="Swipe result" style="width:95%;height:auto" src="data:image/jpeg;base64,{helpers.get_b64string(graph.get_bytes())}"><br><figcaption>{graph_description.text or ''}</figcaption></figure>'''
     return tag
     
   anvil.google.mail.send(
    from_address = "ELI Bis User <bisuserbt@gmail.com>",
    # to = ["petr.zimmermann@eli-beams.eu"],
-   to = ["david.modransky@eli-beams.eu", "petr.pospisil@eli-beams.eu, milan.maly@eli-beams.eu"],
+   to = ["david.modransky@eli-beams.eu", "petr.pospisil@eli-beams.eu", "milan.maly@eli-beams.eu"],
    cc = ["frantisek.vanek@eli-beams.eu", "petr.zimmermann@eli-beams.eu"],
    subject = "ELI Swipe Results",
    html =
